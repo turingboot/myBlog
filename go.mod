ab62b2940d8d7522620a04b967cd3a45ea650abb
Administrator <528164177@qq.com>
2022-11-04 14:30:55 +0800

    后台提交--2022/4/4 14.30

--- go.mod
+++ go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/spf13/viper v1.13.0
 	gorm.io/driver/mysql v1.4.3
-	gorm.io/gorm v1.24.0
+	gorm.io/driver/sqlite v1.4.2
+	gorm.io/gen v0.3.18
+	gorm.io/gorm v1.24.1
+	gorm.io/plugin/dbresolver v1.3.0
 )
 
 require (
@@ -25,6 +28,7 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-sqlite3 v1.14.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -37,11 +41,15 @@ require (
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/mod v0.6.0 // indirect
 	golang.org/x/net v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/tools v0.2.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/datatypes v1.0.7 // indirect
+	gorm.io/hints v1.1.1 // indirect
 )
--- service/user.go
+++ service/user.go
@@ -2,32 +2,37 @@ package service
 
 import (
 	"myBlog/common/errmsg"
-	"myBlog/models"
+	"myBlog/common/global"
+	"myBlog/dal/model"
+	"myBlog/dal/query"
 )
 
 type UserService struct {
 }
 
-func (us *UserService) UserinfoInt(ID int) (models.User, int) {
-	model := models.GetUserById(ID)
-	if model.Username == "" {
-		return model, errmsg.ERROR_USER_NOT_EXIST
+func (us *UserService) UserinfoInt(ID int) (model.User, int) {
+	var u = query.Use(global.Db).User
+	user, _ := u.GetUserById(ID)
+	if user.Username == "" {
+		return user, errmsg.ERROR_USER_NOT_EXIST
 	}
-	return model, errmsg.SUCCSE
+	return user, errmsg.SUCCSE
 }
 
-func (us *UserService) UserinfoStr(userName string) (models.User, int) {
-	model := models.GetUserByName(userName)
-	if model.Username == "" {
-		return model, errmsg.ERROR_USER_NOT_EXIST
+func (us *UserService) UserinfoStr(userName string) (model.User, int) {
+	var u = query.Use(global.Db).User
+	user, _ := u.GetUserByName(userName)
+	if user.Username == "" {
+		return user, errmsg.ERROR_USER_NOT_EXIST
 	}
-	return model, errmsg.SUCCSE
+	return user, errmsg.SUCCSE
 }
 
-func (us *UserService) UserList() ([]models.User, int) {
-	modelList := models.GetUserList()
-	if modelList == nil {
-		return modelList, errmsg.ERROR_USER_NOT_EXIST
+func (us *UserService) UserList() ([]model.User, int) {
+	var u = query.Use(global.Db).User
+	userList, _ := u.GetUserList()
+	if userList == nil {
+		return userList, errmsg.ERROR_USER_NOT_EXIST
 	}
-	return modelList, errmsg.SUCCSE
+	return userList, errmsg.SUCCSE
 }
